cf6e6c717913e549eca81c3ac8c3170fed0d327e
김현범 <hb0636@naver.com>
2021-06-07 21:03:00 +0900

    AlarmForm, ScheduleForm UI 완료

--- AlarmProgram/AlarmForm.Designer.cs
+++ AlarmProgram/AlarmForm.Designer.cs
@@ -29,19 +29,229 @@ namespace AlarmProgram
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnAlrAdd = new System.Windows.Forms.Button();
+            this.btnAlrChg = new System.Windows.Forms.Button();
+            this.btnAlrDel = new System.Windows.Forms.Button();
+            this.AlarmBox = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAllReset = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.AlarmBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAlrDel);
+            this.groupBox1.Controls.Add(this.btnAlrChg);
+            this.groupBox1.Controls.Add(this.btnAlrAdd);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(319, 228);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "알람 추가/수정/삭제";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "오전",
+            "오후"});
+            this.comboBox1.Location = new System.Drawing.Point(89, 142);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(89, 113);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(200, 23);
+            this.numericUpDown2.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "오전/오후";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "분";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(89, 84);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(200, 23);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "시간";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "날짜";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "알람 이름";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(89, 55);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // btnAlrAdd
+            // 
+            this.btnAlrAdd.Location = new System.Drawing.Point(24, 191);
+            this.btnAlrAdd.Name = "btnAlrAdd";
+            this.btnAlrAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAlrAdd.TabIndex = 9;
+            this.btnAlrAdd.Text = "추가";
+            this.btnAlrAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnAlrChg
+            // 
+            this.btnAlrChg.Location = new System.Drawing.Point(120, 191);
+            this.btnAlrChg.Name = "btnAlrChg";
+            this.btnAlrChg.Size = new System.Drawing.Size(75, 23);
+            this.btnAlrChg.TabIndex = 10;
+            this.btnAlrChg.Text = "수정";
+            this.btnAlrChg.UseVisualStyleBackColor = true;
+            // 
+            // btnAlrDel
+            // 
+            this.btnAlrDel.Location = new System.Drawing.Point(214, 191);
+            this.btnAlrDel.Name = "btnAlrDel";
+            this.btnAlrDel.Size = new System.Drawing.Size(75, 23);
+            this.btnAlrDel.TabIndex = 11;
+            this.btnAlrDel.Text = "삭제";
+            this.btnAlrDel.UseVisualStyleBackColor = true;
+            // 
+            // AlarmBox
+            // 
+            this.AlarmBox.Controls.Add(this.dataGridView1);
+            this.AlarmBox.Location = new System.Drawing.Point(356, 13);
+            this.AlarmBox.Name = "AlarmBox";
+            this.AlarmBox.Size = new System.Drawing.Size(540, 470);
+            this.AlarmBox.TabIndex = 1;
+            this.AlarmBox.TabStop = false;
+            this.AlarmBox.Text = "알람 목록";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(7, 23);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(527, 441);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnAllReset
+            // 
+            this.btnAllReset.Location = new System.Drawing.Point(101, 335);
+            this.btnAllReset.Name = "btnAllReset";
+            this.btnAllReset.Size = new System.Drawing.Size(122, 42);
+            this.btnAllReset.TabIndex = 6;
+            this.btnAllReset.Text = "초기화";
+            this.btnAllReset.UseVisualStyleBackColor = true;
             // 
             // AlarmForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(908, 495);
+            this.Controls.Add(this.btnAllReset);
+            this.Controls.Add(this.AlarmBox);
+            this.Controls.Add(this.groupBox1);
             this.Name = "AlarmForm";
             this.Text = "알람 추가";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.AlarmBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAlrDel;
+        private System.Windows.Forms.Button btnAlrChg;
+        private System.Windows.Forms.Button btnAlrAdd;
+        private System.Windows.Forms.GroupBox AlarmBox;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAllReset;
     }
 }
--- AlarmProgram/ScheduleForm.Designer.cs
+++ AlarmProgram/ScheduleForm.Designer.cs
@@ -29,19 +29,165 @@ namespace AlarmProgram
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.btnSchDel = new System.Windows.Forms.Button();
+            this.btnSchChg = new System.Windows.Forms.Button();
+            this.btnSchAdd = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSchDel);
+            this.groupBox1.Controls.Add(this.btnSchChg);
+            this.groupBox1.Controls.Add(this.btnSchAdd);
+            this.groupBox1.Controls.Add(this.dateTimePicker2);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(285, 198);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "일정 추가/수정/삭제";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "일정 이름";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "시작일";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "종료일";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(71, 58);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(71, 93);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker2.TabIndex = 5;
+            // 
+            // btnSchDel
+            // 
+            this.btnSchDel.Location = new System.Drawing.Point(199, 169);
+            this.btnSchDel.Name = "btnSchDel";
+            this.btnSchDel.Size = new System.Drawing.Size(75, 23);
+            this.btnSchDel.TabIndex = 14;
+            this.btnSchDel.Text = "삭제";
+            this.btnSchDel.UseVisualStyleBackColor = true;
+            // 
+            // btnSchChg
+            // 
+            this.btnSchChg.Location = new System.Drawing.Point(105, 169);
+            this.btnSchChg.Name = "btnSchChg";
+            this.btnSchChg.Size = new System.Drawing.Size(75, 23);
+            this.btnSchChg.TabIndex = 13;
+            this.btnSchChg.Text = "수정";
+            this.btnSchChg.UseVisualStyleBackColor = true;
+            // 
+            // btnSchAdd
+            // 
+            this.btnSchAdd.Location = new System.Drawing.Point(9, 169);
+            this.btnSchAdd.Name = "btnSchAdd";
+            this.btnSchAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnSchAdd.TabIndex = 12;
+            this.btnSchAdd.Text = "추가";
+            this.btnSchAdd.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(325, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(571, 471);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "일정 목록";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(7, 23);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(558, 442);
+            this.dataGridView1.TabIndex = 0;
             // 
             // ScheduleForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(908, 495);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "ScheduleForm";
             this.Text = "일정 추가";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSchDel;
+        private System.Windows.Forms.Button btnSchChg;
+        private System.Windows.Forms.Button btnSchAdd;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
