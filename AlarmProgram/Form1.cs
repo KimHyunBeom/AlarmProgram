e9a529dacbe4d792ab76d06ce3766f27de861b53
김현범 <hb0636@naver.com>
2021-06-07 23:28:25 +0900

    DataManager 클래스 생성, Form1 임시 데이터테이블 생성

--- AlarmProgram/Form1.cs
+++ AlarmProgram/Form1.cs
@@ -15,6 +15,51 @@ namespace AlarmProgram
         public Form1()
         {
             InitializeComponent();
+
+            //속성
+            DataTable dt1 = new DataTable();
+            dt1.Columns.Add("일정 이름");
+            dt1.Columns.Add("시작일");
+            dt1.Columns.Add("종료일");
+            dt1.Columns.Add("남은 시간");
+            //열 선언
+            DataRow dr1 = dt1.NewRow();
+            //dt.Rows.Add(dr);
+
+            dt1.AcceptChanges();
+
+            this.dataGridView1.DataSource = dt1;
+
+            //속성
+            DataTable dt2 = new DataTable();
+            dt2.Columns.Add("알람 이름");
+            dt2.Columns.Add("알람 시간");
+            dt2.Columns.Add("남은 시간");
+            //열 선언
+            DataRow dr2 = dt1.NewRow();
+            //dt.Rows.Add(dr);
+
+            dt2.AcceptChanges();
+
+            this.dataGridView2.DataSource = dt2;
+        }
+
+        private void ScheduleDataTable()
+        {
+            /*
+            DataTable dt = new DataTable();
+            dt.Columns.Add("일정 이름");
+            dt.Columns.Add("시작일");
+            dt.Columns.Add("종료일");
+            dt.Columns.Add("남은 시간");
+
+            DataRow dr = dt.NewRow();
+            dt.Rows.Add(dr);
+
+            dt.AcceptChanges();
+
+            this.dataGridView1.DataSource = dt;
+            */
         }
 
         //메뉴 알람 관리 항목의 알람 추가 버튼 클릭 시
